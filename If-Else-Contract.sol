b79aed84802a72e5973504c1ae7b7068cda73f7c
Trent M <59753390+Trent122@users.noreply.github.com>
2021-11-10 08:45:13 -0700

    Create If-Else-Contract.sol

---
+++ If-Else-Contract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.3;
+
+contract ifElse {
+    function foo(uint ) public {
+        if (msg.value >= 0) {
+            msg.sender.transfer(msg.value);
+        } else {
+            revert("negative value");
+        }
+    }
+
+    function ternary (uint _x) public {
+        uint x = _x > 0 ? _x : 0;
+    }
+    
+   // Solidity support conditional statements if, else if and else. 
