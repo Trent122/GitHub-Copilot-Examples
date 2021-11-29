8d15a1f1a17e3f1578ee40e9578c5833aa916188
Trent M <59753390+Trent122@users.noreply.github.com>
2021-11-29 11:58:15 -0700

    Create Visibility Contracts.sol

---
+++ Visibility Contracts.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.3;
+
+contract Base {
+    // Private function can only be called
+    // - inside this contract
+    // Contracts that inherit this contract cannot call this function.
+    function privateFunc() private pure returns (string memory) {
+        return "private function called";
+    }
